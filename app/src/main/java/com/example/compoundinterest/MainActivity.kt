12544e84ea2314ae8b225efe470d8a1c744c663b
Ayo-mi <ajayijubrilayo@gmail.com>
2023-10-12 10:52:49 +0100

    working

--- app/src/main/java/com/example/compoundinterest/MainActivity.kt
+++ app/src/main/java/com/example/compoundinterest/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.compoundinterest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.compoundinterest.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+import java.text.DecimalFormat
 import kotlin.math.pow
 
 class MainActivity : AppCompatActivity() {
@@ -17,18 +20,25 @@ class MainActivity : AppCompatActivity() {
 
         vieww.btnCal.setOnClickListener {
             try {
-            val p = vieww.principalTxt.text.toString().toDouble()
-            val rate = vieww.rateTxt.text.toString().toDouble()
-            val time = vieww.timeTxt.text.toString().toDouble()
-            val n = 12
-            var s = 1 + (rate/n)
-            s = s.pow(n * time)
-            val comp = (p * s).toFloat()
-            val initVal = (comp * p).toFloat()
+                val p = vieww.principalTxt.text.toString().toDouble()
+                val rate = vieww.rateTxt.text.toString().toDouble()
+                val time = vieww.timeTxt.text.toString().toDouble()
+                val n = 365
+                var s = 1 + (rate/n)
+                s = s.pow(n * time)
+                val comp = (p * s).toFloat()
+                val initVal = (comp * p).toFloat()
+                val dec = DecimalFormat("#,###.00")
 
-            vieww.resTxt.text = "Compound Interest: $comp\nInitial Value: $initVal"
+                vieww.timeTxt.clearFocus()
+                vieww.principalTxt.clearFocus()
+                vieww.rateTxt.clearFocus()
+
+                vieww.resTxt.text = "Compound Interest: \n$${dec.format(comp)}\n\nInitial Value: \n$${dec.format(initVal)}"
             }catch (e: Exception){
-                vieww.resTxt.text = "Invalid action"
+                //vieww.resTxt.text = "Invalid action"
+                Snackbar.make(vieww.root, "Invalid action", Snackbar.LENGTH_LONG).show()
+                //Toast.makeText(this, "Invalid action", Toast.LENGTH_LONG).show()
             }
         }
 
