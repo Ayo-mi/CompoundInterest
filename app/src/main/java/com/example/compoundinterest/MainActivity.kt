ed80e9ca4008b8168b2b75b2c756e886abeb741e
Ayo-mi <ajayijubrilayo@gmail.com>
2023-10-12 12:03:10 +0100

    fix error in calculations

--- app/src/main/java/com/example/compoundinterest/MainActivity.kt
+++ app/src/main/java/com/example/compoundinterest/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.example.compoundinterest
 
+import android.annotation.SuppressLint
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.inputmethod.InputMethodManager
-import android.widget.Toast
 import com.example.compoundinterest.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 import java.text.DecimalFormat
@@ -12,6 +12,7 @@ import kotlin.math.pow
 
 class MainActivity : AppCompatActivity() {
     private lateinit var vieww: ActivityMainBinding
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //initialize the binding object here
@@ -28,22 +29,19 @@ class MainActivity : AppCompatActivity() {
                 val n = 365
                 var s = 1 + (rate/n)
                 s = s.pow(n * time)
-                val comp = (p * s).toFloat()
-                val initVal = (comp * p).toFloat()
+                val comp = ((p * s) - p).toFloat()
+                val initVal = (comp + p).toFloat()
                 val dec = DecimalFormat("#,###.00")
 
                 closeKeyBoard()
 
                 vieww.resTxt.text = "Compound Interest: \n$${dec.format(comp)}\n\nInitial Value: \n$${dec.format(initVal)}"
             }catch (e: Exception){
-                //vieww.resTxt.text = "Invalid action"
                 Snackbar.make(vieww.root, "Invalid action", Snackbar.LENGTH_LONG).show()
-                //Toast.makeText(this, "Invalid action", Toast.LENGTH_LONG).show()
             }
         }
 
     }
-
     private fun closeKeyBoard() {
         val view = this.currentFocus
         if (view != null) {
